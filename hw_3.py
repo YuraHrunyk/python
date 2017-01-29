68b944989cbd4e6d6b8fa12420f765979da37af1
YuraHrunyk <grynukyura@gmail.com>
2017-01-29 23:30:08 +0200

    Update hw_3.py

--- hw_3.py
+++ hw_3.py
@@ -9,7 +9,6 @@ path = sys.argv[4]
 prefix = sys.argv[5]
 counts = int(sys.argv[6])
 mode = int(sys.argv[7], 8)
-command = sys.argv[8]
 
 print(host)
 
@@ -21,7 +20,7 @@ ssh.connect(host, port=port, username=name)
 i = 0
 while i < counts:
 	fld = os.path.join(path, prefix + str(i))
-	stdin, stdout, stderr = ssh.exec_command(command + fld)
+	stdin, stdout, stderr = ssh.exec_command('mkdir ' + fld)
 	stdin, stdout, stderr = ssh.exec_command('chmod ' + str(mode) + ' ' + fld)
 	i = i + 1
 
