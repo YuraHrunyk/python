d9768f2ae0974fc4031fe8c6ec3a07c6a0c8d0e8
YuraHrunyk <grynukyura@gmail.com>
2017-02-15 14:37:28 +0200

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -5,20 +5,20 @@ import sqlite3
 path = sys.argv[1]
 
 
-sql = '''SELECT ServerPorts.port_number
-		FROM ServerTypes
-		INNER JOIN Servers
-		ON ServerTypes.id = Servers.servertypes_id
-		INNER JOIN ServerPorts
-		ON ServerPorts.servers_id = Servers.id
-		INNER JOIN ServerProjects
-		ON ServerProjects.servers_id = Servers.id
-		WHERE ServerTypes.type_name = 'apache' AND ServerProjects.projects_id = '3';'''
+sql = '''UPDATE ServerPorts
+                SET port_number = '443'
+                WHERE id in (SELECT ServerPorts.id
+                FROM ServerTypes
+                INNER JOIN Servers
+                ON ServerTypes.id = Servers.servertypes_id
+                INNER JOIN ServerPorts
+                ON ServerPorts.servers_id = Servers.id
+                INNER JOIN ServerProjects
+                ON ServerProjects.servers_id = Servers.id
+                WHERE ServerTypes.type_name = 'apache' AND ServerProjects.projects_id = 3);'''
 conn = sqlite3.connect(path)
-result = conn.execute(sql).fetchall()
+conn.execute(sql).fetchall()
 conn.commit()
 
-for row in result:
-	print('PortNumber= ' + str(row[0]))
-
 conn.close()
+
