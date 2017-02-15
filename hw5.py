d683730dd64b94a57f22da625792375b1d46c58b
YuraHrunyk <grynukyura@gmail.com>
2017-02-15 09:09:30 +0200

    Update and rename hw5 to hw5.py

---
+++ hw5.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import sqlite3
+
+path = sys.argv[1]
+
+
+sql = '''SELECT ServerPorts.port_number
+		FROM ServerTypes
+		INNER JOIN Servers
+		ON ServerTypes.id = Servers.servertypes_id
+		INNER JOIN ServerPorts
+		ON ServerPorts.servers_id = Servers.id
+		INNER JOIN ServerProjects
+		ON ServerProjects.servers_id = Servers.id
+		WHERE ServerTypes.type_name = 'apache' AND ServerProjects.projects_id = '3';'''
+conn = sqlite3.connect(path)
+result = conn.execute(sql).fetchall()
+commit = conn.commit()
+
+for row in result:
+	print('PortNumber= ' + str(row[0]))
