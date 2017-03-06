6ce4a61d8ebc9eeb18d740cc8ceb53b9f0693c46
YuraHrunyk <grynukyura@gmail.com>
2017-03-07 01:12:52 +0200

    Update hw6.py

--- hw6.py
+++ hw6.py
@@ -10,9 +10,11 @@ docker_conn = dockermap.api.DockerClientWrapper(docker_url)
 docker_file = dockermap.api.DockerFile('centos7/hw', maintainer='Hrunyk Yurii')
 docker_file.run_all('yum -y update')
 docker_file.run_all('yum -y install httpd')
-docker_file.run_all('systemctl start httpd')
-docker_file.run('echo "Homework6!" > /var/www/html/index.html')
+docker_file.run_all('echo Homework6! > /var/www/html/index.html')
 
+docker_file.entrypoint = ['/usr/sbin/httpd']
+docker_file.command = ['-D', 'FOREGROUND']
 
 docker_conn.build_from_file(docker_file, tag_name)
+
 
